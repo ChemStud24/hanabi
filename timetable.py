e59988c9043e8ed055a9abaa60d4f0743123a3ec
Will Johnson <will.johnson@yale.edu>
2022-04-15 19:38:30 -0400

    update

--- timetable.py
+++ timetable.py
@@ -1,4 +1,6 @@
 from DD_test import run_game
+from itertools import permutations
+from time import time
 
 if __name__ == "__main__":
   # Check that the cdef and library were loaded from the standard paths.
@@ -9,5 +11,10 @@ if __name__ == "__main__":
   iterations = 10000
   for r in range(1,6):
   	for c in range(1,6):
-  		ncards = r*c
-		run_game({"players": 2, "random_start_player": False,"colors":int(sys.argv[2])},int(sys.argv[1]))
+  		ncards = 2*r*c
+  		nworlds = len(permutations(range(ncards-5),5))
+  		t0 = time()
+		run_game({"players": 2, "random_start_player": False,"colors":c,"ranks":r},iterations)
+		t = time() - t0
+
+		print("Ranks: " + str(r) + " Colors: " + str(c) + " Worlds: " + str(nworlds) + " Time: " + str(nworlds/iterations*t))
