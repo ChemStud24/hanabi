5a5ca74db174753ba39345de10cbfa90c45d892f
Will Johnson <will.johnson@yale.edu>
2022-04-15 10:12:52 -0400

    update

--- playing_around.py
+++ playing_around.py
@@ -61,14 +61,14 @@ def run_game(game_parameters):
     print_state(state2)
 
     # print(state.observation(1).observed_hands()[1])
-    print(card.to_dict() for card in state.observation(0).observed_hands()[1])
+    print((card.to_dict() for card in state.observation(0).observed_hands()[1]))
 
     # hands = state.player_hands()
     # for pid,hand in enumerate(hands):
     #   print(hand)
     #   state2.set_hand(pid,hand)
 
-    state2.set_hand(1,card.to_dict() for card in state.observation(0).observed_hands()[1])
+    state2.set_hand(1,(card.to_dict() for card in state.observation(0).observed_hands()[1]))
 
     print(state)
     print(state2)
