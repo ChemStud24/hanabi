79778113d4b18cd59cfab70e0766bac7f91e347d
Will Johnson <will.johnson@yale.edu>
2022-04-16 10:35:52 -0400

    update

--- DD_test.py
+++ DD_test.py
@@ -72,13 +72,16 @@ def run_all_games(game_parameters):
   cum_score = 0
   perfects = 0
 
+
+  counter = 0
   init_state = game.new_initial_state()
   for state in all_worlds(game,init_state):
     while not state.is_terminal():
       if state.cur_player() == pyhanabi.CHANCE_PLAYER_ID:
         state.deal_random_card()
         continue
-
+      counter += 1
+      print(counter)
       move = double_dummy_action(state)
       state.apply_move(move)
     # print(state)
