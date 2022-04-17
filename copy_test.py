5cc7301b167270a87cf7f2997844e40948f4bf68
Will Johnson <will.johnson@yale.edu>
2022-04-17 16:06:38 -0400

    copy_test

--- copy_test.py
+++ copy_test.py
@@ -2,6 +2,6 @@ from hanabi_learning_environment.pyhanabi import HanabiGame
 
 g = HanabiGame()
 s1 = g.new_initial_state()
-print(s1.fireworks)
+print(s1.fireworks())
 s2 = s1.copy()
-print(s2.fireworks)
+print(s2.fireworks())
