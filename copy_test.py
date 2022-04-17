786bd20076110e79c0bc4a1feb28386dde2439fe
Will Johnson <will.johnson@yale.edu>
2022-04-17 16:41:26 -0400

    copy_test

--- copy_test.py
+++ copy_test.py
@@ -9,7 +9,7 @@ from hanabi_learning_environment.pyhanabi import HanabiGame
 for r in range(1,6):
 	for c in range(1,6):
 		ncards = 2*r*c
-		if ncards < 10:
+		if ncards < 10 or (r == 2 and c == 3):
 			continue
 		print("Colors: " + str(c))
 		print("Ranks: " + str(r))
@@ -17,8 +17,5 @@ for r in range(1,6):
 		s1 = g.new_initial_state()
 		print(s1.fireworks())
 		s2 = s1.copy()
-		try:
-			print(s2.fireworks())
-		except:
-			print("Core Dumped :/")
+		print(s2.fireworks())
 		print("")
