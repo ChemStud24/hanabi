ecdb41906db8450253c682e8f268d7f6a34d42e9
Will Johnson <will.johnson@yale.edu>
2022-04-17 16:24:18 -0400

    copy_test

--- copy_test.py
+++ copy_test.py
@@ -7,13 +7,13 @@ from hanabi_learning_environment.pyhanabi import HanabiGame
 # print(s2.fireworks())
 
 for r in range(1,6):
-  	for c in range(1,6):
-  		ncards = 2*r*c
-  		if ncards < 10:
-  			continue
-  		print("Colors: " + str(c))
-  		print("Ranks: " + str(r))
-  		g = HanabiGame({"colors":c,"ranks":r})
+	for c in range(1,6):
+		ncards = 2*r*c
+		if ncards < 10:
+			continue
+		print("Colors: " + str(c))
+		print("Ranks: " + str(r))
+		g = HanabiGame({"colors":c,"ranks":r})
 		s1 = g.new_initial_state()
 		print(s1.fireworks())
 		s2 = s1.copy()
