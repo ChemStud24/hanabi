93e7b3e23e0f16dea6a66b43bc010eaae96984d5
Will Johnson <will.johnson@yale.edu>
2022-04-18 13:54:46 -0400

    PIMC

--- utils.py
+++ utils.py
@@ -106,6 +106,9 @@ def all_cards(game):
 				yield {'color':color_idx_to_char(c),'rank':r}
 			if r == 0:
 				yield {'color':color_idx_to_char(c),'rank':r}
+			if game.num_ranks() == 1:
+				yield {'color':color_idx_to_char(c),'rank':r}
+				yield {'color':color_idx_to_char(c),'rank':r}
 
 def possible_cards(game,observation):
 	# TODO remove firework cards
